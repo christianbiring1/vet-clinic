a97ffe02cd59fce90ec2f6f7ba0f8a1656a20ea9
christian <christianbiringanine22@gmail.com>
2022-07-05 18:16:07 +0200

    arrange queries and data

--- data.sql
+++ data.sql
@@ -3,4 +3,4 @@ INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg) V
 INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Gabumon', '2018-11-15', 2, TRUE, 8);
 INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Pikachu', '2021-01-07', 1, FALSE, 15.04);
 INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Devimon', '2017-05-12', 5, TRUE, 11);
-select name from vet_clinicschema.animals where date_of_birth between '2016-01-01' and '2019-12-31';
+
