d1627d159d5bb0e22ead64d6711fbeea290f166a
christian <christianbiringanine22@gmail.com>
2022-07-05 18:13:09 +0200

    Add sql queries

--- queries.sql
+++ queries.sql
@@ -1,3 +1,9 @@
 /*Queries that provide answers to the questions from all projects.*/
-
-SELECT * from animals WHERE name = 'Luna';
+SELECT name from vet_clinicschema.animals WHERE right (name, 3) = 'mon';
+SELECT name from vet_clinicschema.animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
+SELECT name from vet_clinicschema.animals WHERE neutered = TRUE AND escape_attempts < 3;
+SELECT date_of_birth from vet_clinicschema.animals WHERE name = 'Agumon' OR name = 'Pikachu';
+SELECT name, escape_attempts from vet_clinicschema.animals WHERE weight_kg > 10.5;
+SELECT * from vet_clinicschema.animals WHERE neutered = TRUE;
+SELECT * from vet_clinicschema.animals WHERE name != 'Gabumon';
+SELECT * from vet_clinicschema.animals WHERE 10.4 <= weight_kg AND weight_kg >= 17.3;
