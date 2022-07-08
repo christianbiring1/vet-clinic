2d373bfd48a9261d34e922bdfa5e717026b29449
christian <christianbiringanine22@gmail.com>
2022-07-08 22:01:26 +0200

    Add the queries of the asked question attached as comments

--- queries.sql
+++ queries.sql
@@ -13,3 +13,28 @@ BEGIN;
 ALTER TABLE animals RENAME COLUMN species TO unspecified;
 SELECT * FROM animals;
 ROLLBACK;
+
+-- How many animals are there?
+SELECT COUNT (name) FROM animals;
+
+-- How many animals have never tried to escape?
+SELECT COUNT (name) FROM animals WHERE escape_attempts = 0;
+
+-- What is the average weight of animals?
+SELECT AVG (weight_kg) FROM animals;
+
+-- Who escapes the most, neutered or not neutered animals?
+SELECT MAX (escape_attempts) FROM animals;
+SELECT name FROM animals WHERE escape_attempts >= 7;
+SELECT name FROM animals WHERE neutered = TRUE;
+SELECT name FROM animals WHERE neutered = FALSE;
+
+-- What is the minimum and maximum weight of each type of animal?
+SELECT MIN (weight_kg) FROM animals WHERE species = 'pokemon';
+SELECT MAX (weight_kg) FROM animals WHERE species = 'pokemon';
+SELECT MIN (weight_kg) FROM animals WHERE species = 'digimon';
+SELECT MAX (weight_kg) FROM animals WHERE species = 'digimon';
+
+-- What is the average number of escape attempts per animal type of those born between 1990 and 2000?
+SELECT AVG (escape_attempts) FROM animals WHERE species = 'digimon' AND date_of_birth between '1990-01-01' AND '2000-12-31';
+SELECT AVG (escape_attempts) FROM animals WHERE species = 'pokemon' AND date_of_birth between '1990-01-01' AND '2000-12-31';
